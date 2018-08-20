1213733dd1ccd70eb5ca94aa5e7c8e078325c8c3
VinzenzBildstein <vbildste@uoguelph.ca>
2018-08-20 12:00:47 -0400

    Added version header and script

--- src/TriumfLibrary.cxx
+++ src/TriumfLibrary.cxx
@@ -2,9 +2,12 @@
 
 #include "TMidasFile.h"
 #include "TTriumfDataParser.h"
+#include "GRSIDataVersion.h"
 
 extern "C" TMidasFile* CreateFile(std::string& fileName) { return new TMidasFile(fileName.c_str()); }
 extern "C" void DestroyFile(TMidasFile* obj) { delete obj; }
 
 extern "C" TTriumfDataParser* CreateParser() { return new TTriumfDataParser; }
 extern "C" void DestroyParser(TTriumfDataParser* obj) { delete obj; }
+
+extern "C" std::string LibraryVersion() { return std::string(GRSIDATA_RELEASE); }
