9b2c6fdc51077c622f5046a99e44502df6b3bc7b
VinzenzBildstein <vbildste@uoguelph.ca>
2024-01-31 12:01:54 -0500

    Initial working version of TGriffinAngles

--- include/TGriffinAngles.h
+++ include/TGriffinAngles.h
@@ -19,29 +19,39 @@
 #include <map>
 #include <functional>
 
-#include "TObject.h"
+#include "TNamed.h"
 
-class TGriffinAngles : public TObject {
+class TGriffinAngles : public TNamed {
 public:
-	TGriffinAngles(double distance, bool folding, bool grouping, bool addback);
+	TGriffinAngles(double distance = 145., bool folding = false, bool grouping = false, bool addback = true);
 	~TGriffinAngles() {}
+
+	double Distance() { return fDistance; }
+	double Folding() { return fFolding; }
+	double Grouping() { return fGrouping; }
+	double Addback() { return fAddback; }
 
 	int Index(double angle);
 	int NumberOfAngles() const { return fAngles.size(); }
 	double Angle(int index) const { auto it = fAngles.begin(); std::advance(it, index); return *it; }
+	double AverageAngle(int index) const;
 
 	std::set<double>::iterator begin() const { return fAngles.begin(); }
 	std::set<double>::iterator end() const { return fAngles.end(); }
 
-	void Print() const;
+	void Print(Option_t* = "") const override;
 
 private:
+	double fDistance{145.}; ///< distance of detector from center of array in mmm
+	bool fFolding{false}; ///< flag indicating whether we fold our distribution around 90 degree 
+	bool fGrouping{false}; ///< flag indicating whether we group close angles together
+	bool fAddback{true}; ///< flag indicating whether we use addback
 	double fRounding{0.01}; ///< we consider any angles whose difference is less than this to be equal
-	std::set<double, std::function<bool(const double&, const double&)>> fAngles; ///< set of unique angles, when grouping is used, the largest angle of the group is used!
+	std::set<double> fAngles; ///< set of unique angles, when grouping is used, the largest angle of the group is used!
 	std::map<double, int> fAngleMap; ///< Maps angles to indices. This is fairly straight forward without grouping, but if grouping is used multiple angles can be mapped to the same index.
 
 	/// \cond CLASSIMP
-	ClassDef(TGriffinAngles, 1)
+	ClassDefOverride(TGriffinAngles, 2)
 	/// \endcond
 };
 /*! @} */
