b0326ff44216b3f7b36565c9eed7e9ef984840ae
dhruval108 <65043682+dhruval108@users.noreply.github.com>
2024-07-30 14:46:03 -0600

    Create LinkDef.h for TRcmp

---
+++ libraries/TGRSIAnalysis/TRcmp/LinkDef.h
@@ -0,0 +1,14 @@
+//TRcmp.h TRcmpHit.h 
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link off nestedclasses;
+
+
+#pragma link C++ class TRcmpHit+;
+#pragma link C++ class std::vector<TRcmpHit>+;
+#pragma link C++ class TRcmp+;
+
+#endif
