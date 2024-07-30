5a980dee87528337b9e6794a6f67cdffcfcb3db5
dhruval108 <65043682+dhruval108@users.noreply.github.com>
2024-07-30 15:23:03 -0600

    Update TRcmp.h

--- include/TRcmp.h
+++ include/TRcmp.h
@@ -49,23 +49,23 @@ public:
    };
 
    // constructors and destructors
-   TRcmp();               // default constructor
-   TRcmp(const TRcmp&);   // copy constructor
-   ~TRcmp() override;     // destructor
+   TRcmp();               ///< default constructor
+   TRcmp(const TRcmp&);   ///< copy constructor
+   ~TRcmp() override;     ///< destructor
 
 #ifndef __CINT__
    void AddFragment(const std::shared_ptr<const TFragment>&, TChannel*) override;   //!<! //adds front and back fragments to their respective fragment vectors
 #endif
-   void BuildHits() override {} // even though we are not using this function, we still need to override it to avoid warning during the sorting process
+   void BuildHits() override {} ///< even though we are not using this function, we still need to override it to avoid warning during the sorting process
 
    // functions to access the individual fragments
-   inline TFragment GetRcmpFrontFrag(const int& i) const { return fFrontPFragVector.at(i); } // returns the front fragment stored at the index
-   inline TFragment GetRcmpBackFrag(const int& i)  const { return fBackNFragVector.at(i); }  // returns the back  fragment stored at the index
+   inline TFragment GetRcmpFrontFrag(const int& i) const { return fFrontPFragVector.at(i); } ///< returns the front fragment stored at the index
+   inline TFragment GetRcmpBackFrag(const int& i)  const { return fBackNFragVector.at(i); }  ///< returns the back  fragment stored at the index
 
    // functions to access the individual hits
-   inline TRcmpHit* GetRcmpFrontHit(const int& i) const { return fFrontPMulVector.at(i); }   // returns a front strip hit in RCMP (this hit can be used to fill histograms when looping over front multiplicity)
-   inline TRcmpHit* GetRcmpBackHit(const int& i)  const { return fBackNMulVector.at(i); }    // returns a back strip hit in RCMP  (this hit can be used to fill histograms when looping over back  multiplicity)
-   inline TRcmpHit* GetRcmpHit(const int& i)      const { return fPixelMulVector.at(i); }    // returns a pixel hit in RCMP       (this hit can be used to fill histograms when looping over pixel multiplicity)
+   inline TRcmpHit* GetRcmpFrontHit(const int& i) const { return fFrontPMulVector.at(i); }   ///< returns a front strip hit in RCMP (this hit can be used to fill histograms when looping over front multiplicity)
+   inline TRcmpHit* GetRcmpBackHit(const int& i)  const { return fBackNMulVector.at(i); }    ///< returns a back strip hit in RCMP  (this hit can be used to fill histograms when looping over back  multiplicity)
+   inline TRcmpHit* GetRcmpHit(const int& i)      const { return fPixelMulVector.at(i); }    ///< returns a pixel hit in RCMP       (this hit can be used to fill histograms when looping over pixel multiplicity)
 
    TRcmp& operator=(const TRcmp&);
    void   Copy(TObject&) const override;              //!<!
@@ -74,18 +74,18 @@ public:
    void   Print(std::ostream& out) const override;    //!<!
 
    // multiplicity functions
-   size_t  GetFrontFragmentMultiplicity() const { return fFrontPFragVector.size(); }  // returns the size of the vector that contains all the front fragments (within a build window)
-   size_t  GetBackFragmentMultiplicity()  const { return fBackNFragVector.size(); }   // returns the size of the vector that contains all the back  fragments (within a build window)
-   Short_t GetFrontMultiplicity();                                                    // returns the size of the vector that contains all the front hits (within a build window)
-   Short_t GetBackMultiplicity();                                                     // returns the size of the vector that contains all the back  hits (within a build window)
-   Short_t GetPixelMultiplicity();                                                    // returns the size of the vector that contains all the pixel hits (within a build window)
+   size_t  GetFrontFragmentMultiplicity() const { return fFrontPFragVector.size(); }  ///< returns the size of the vector that contains all the front fragments (within a build window)
+   size_t  GetBackFragmentMultiplicity()  const { return fBackNFragVector.size(); }   ///< returns the size of the vector that contains all the back  fragments (within a build window)
+   Short_t GetFrontMultiplicity();                                                    ///< returns the size of the vector that contains all the front hits (within a build window)
+   Short_t GetBackMultiplicity();                                                     ///< returns the size of the vector that contains all the back  hits (within a build window)
+   Short_t GetPixelMultiplicity();                                                    ///< returns the size of the vector that contains all the pixel hits (within a build window)
 
-   inline Int_t GetZeroMultiplicity() const { return fZeroMultiplicity; }  // returns the zero multiplicty counter
+   inline Int_t GetZeroMultiplicity() const { return fZeroMultiplicity; }  ///< returns the zero multiplicty counter
 
    // note that the coincidence time condition is not implemented in the pixel multiplicity function as of now
    // it can be set in the GRSIFrame helper if needed
-   inline Int_t GetCoincidenceTime() const { return fCoincidenceTime; }                     // returns the coincidence time between front and back strip of a detector in nanoseconds
-   inline void  SetCoincidenceTime(const int coincTime) { fCoincidenceTime = coincTime; }   // sets the coincidence time between front and back strip of a detector in nanoseconds
+   inline Int_t GetCoincidenceTime() const { return fCoincidenceTime; }                     ///< returns the coincidence time between front and back strip of a detector in nanoseconds
+   inline void  SetCoincidenceTime(const int coincTime) { fCoincidenceTime = coincTime; }   ///< sets the coincidence time between front and back strip of a detector in nanoseconds
 
 private:
    // vectors that contain front and back fragments
@@ -109,7 +109,7 @@ private:
    Int_t fCoincidenceTime = 50;   ///< GetTime for TDetectorHit is in nanoseconds, so this is in nanoseconds, too!
 
    /// \cond CLASSIMP
-   ClassDefOverride(TRcmp, 2)
+   ClassDefOverride(TRcmp, 2) // NOLINT
    /// \endcond
 };
 
