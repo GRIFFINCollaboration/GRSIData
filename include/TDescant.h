e6decd90723ec11f65e37a0ce3a176be421e9986
VinzenzBildstein <vbildste@uoguelph.ca>
2024-08-30 14:11:17 -0400

    Fixing readability-redundant-access-specifiers

--- include/TDescant.h
+++ include/TDescant.h
@@ -42,22 +42,22 @@ public:
 #endif
 	void BuildHits() override {} // no need to build any hits, everything already done in AddFragment
 
-public:
    static bool SetWave() { return fSetWave; }         //!<!
    void SetWave(bool flag) { fSetWave = flag; }       //!<!
    bool              Hit() const { return fHitFlag; }       //!<!
    void SetHit(bool flag = true) { fHitFlag = flag; } //!<!
+
+   void Copy(TObject&) const override;            //!<!
+   void Clear(Option_t* opt = "") override;       //!<!
+   void Print(Option_t* opt = "") const override; //!<!
+	void Print(std::ostream& out) const override; //!<!
+
 private:
    static bool              fSetWave;     ///<  Flag for Waveforms ON/OFF
    bool                     fHitFlag;   ///<   Is there a Descant hit?
 
    static std::array<TVector3, 71> fPosition;          //!<!
    static std::array<TVector3, 9>  fAncillaryPosition; //!<!
-public:
-   void Copy(TObject&) const override;            //!<!
-   void Clear(Option_t* opt = "") override;       //!<!
-   void Print(Option_t* opt = "") const override; //!<!
-	void Print(std::ostream& out) const override; //!<!
 
    /// \cond CLASSIMP
    ClassDefOverride(TDescant, 1) // NOLINT(readability-else-after-return)
