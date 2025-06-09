2bf44ea1bd0ef50f6a731565cbeada40c5e0c7c2
Daniel Yates <yates@phas.ubc.ca>
2025-06-09 16:55:20 -0700

    removing superfluous GetPosition in TDemandHit

--- libraries/TGRSIAnalysis/TDemand/TDemandHit.cxx
+++ libraries/TGRSIAnalysis/TDemand/TDemandHit.cxx
@@ -78,18 +78,6 @@ void TDemandHit::Print(std::ostream& out) const
    out << str.str();
 }
 
-TVector3 TDemandHit::GetPosition(int DetNbr) const
-{
-
-   if (1 > DetNbr || 6 < DetNbr){ //if detector number is not 1,2,3,4,5,6, then return a 0 vector
-      return TVector3(0,0,0);
-   }
-
-   else{
-      return TVector3(fXPositions[DetNbr-1],fYPositions[DetNbr-1],fZPositions[DetNbr-1]); //-1 because we index the detectors at 1 but the vector at 0
-   }
-}
-
 TVector3 TDemandHit::GetPosition() const
 {
    return TDemand::GetPosition(*this);
